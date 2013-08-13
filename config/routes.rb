4ffa7b1805a7971a8813f930c3c675fcea00ad7a
KeithAsazawa <keith.asazawa@gmail.com>
2013-08-12 22:23:07 -0700

    Bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 PinterestApp::Application.routes.draw do
 
+  get "users/show"
+
   resources :pins
 
-
   devise_for :users
+  match 'users/:id' => 'users#show', as: :user
 
   root :to => 'pins#index'
 
